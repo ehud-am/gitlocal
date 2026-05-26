aa189ae13f391a46c17929dd8a3d6ae9f6593c07
ehud <ehud@private>
2026-05-25 23:28:11 -0400

    Point 0.9.1 cask at tagged release

--- packaging/macos/cask/gitlocal.rb
+++ packaging/macos/cask/gitlocal.rb
@@ -2,7 +2,7 @@ cask "gitlocal" do
   version "0.9.1"
   sha256 "3017f87dabf9dd70a602d7164e9d265344b208cb661dd16a113d7a2bd6ba6c8d"
 
-  url "https://github.com/ehud-am/gitlocal/releases/download/0.9.1/GitLocal-0.9.1-macos.zip"
+  url "https://github.com/ehud-am/gitlocal/releases/download/v0.9.1/GitLocal-0.9.1-macos.zip"
   name "GitLocal"
   desc "Native macOS repository viewer for GitLocal"
   homepage "https://github.com/ehud-am/gitlocal"
